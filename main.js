049e60de443b81be8dcae977f297abb2960efdac
Scott Bouchard <holdwhatdoor@gmail.com>
2023-05-12 23:04:50 -0700

    Update main.js
    changed '=' to '<' in for loop to fix endless loop issue

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ var input = 'turpentine and turtles';
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 var resultArray = [];
 
-for(let i = 0; i = input.length; i++){
+for(let i = 0; i < input.length; i++){
   let phraseLetter = input[i];
   // console.log(`Input letter: ${phraseLetter}, at index ${i}`);
+  for(let j = 0; j < vowels.length; j++){
+        let vowel = vowels[j];
+        // console.log(`Letter '${phraseLetter}' at index '${i}' in Outer array/phrase.`);
+        // console.log(`Index '${j}' of vowel '${vowel}' in Inner vowel array.`);
+        if(phraseLetter === vowel){
+            // --console.log(`Outer Array letter: '${phraseLetter}' at index ${i}, matches Inner Array vowel '${vowel}' at  index '${j}'`);
+         }
+    }
 }
+
+
