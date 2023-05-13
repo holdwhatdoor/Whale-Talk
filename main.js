937989277ade3c0c4e7023fd917389130f39c3e7
Scott Bouchard <holdwhatdoor@gmail.com>
2023-05-12 22:38:28 -0700

    Create main.js
    Declared initial phrase, vowel array and empty result array and wrote for loop to iterate through the initial input phrase to be translated.

---
+++ main.js
@@ -0,0 +1,12 @@
+// Take a phrase like ‘turpentine and turtles’ and translate it into its “whale talk” equivalent: ‘UUEEIEEAUUEE’
+// 
+
+
+var input = 'turpentine and turtles';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+var resultArray = [];
+
+for(let i = 0; i = input.length; i++){
+  let phraseLetter = input[i];
+  // console.log(`Input letter: ${phraseLetter}, at index ${i}`);
+}
